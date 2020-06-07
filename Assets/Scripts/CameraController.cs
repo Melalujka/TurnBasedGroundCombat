aa45141897038b8050d33d178a2edee932e854ff
pavel <pavel.antonov1980@icloud.com>
2020-06-08 00:13:07 +0200

    basic characters movement added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = GetBehindPosition(player, 20, 20);
+       // gameObject.transform.rotation = player.rotation + new Vector3(-30, 30, -30);
+        gameObject.transform.LookAt(player.position);
+    }
+
+    Vector3 GetBehindPosition(Transform target, float distanceBehind, float distanceAbove)
+    {
+        return target.position - (target.forward * distanceBehind) + (target.up * distanceAbove);
+    }
+}
