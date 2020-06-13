7bf79b12d0e2ce8504983331a303523160a8bd78
pavel <pavel.antonov1980@icloud.com>
2020-06-14 01:45:40 +0200

    simple cast range checker added

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -14,24 +14,26 @@ public class BattleManager : MonoBehaviour
 
     public CharacterController[] ActiveCharacters => GetCharactersRange(isTopTurn);
 
+    public CharacterController[] InactiveCharacters => GetCharactersRange(!isTopTurn);
+
     public bool isTopTurn = true;
 
-    private bool shotState = false;
+    //private bool shotState = false;
 
     [SerializeField] BattleUI battleUI;
 
     void Start()
     {
-        battleUI.shotButton.onClick.AddListener(ChangeShotState);
+        //battleUI.shotButton.onClick.AddListener(ChangeShotState);
     }
 
-    void ChangeShotState()
-    {
-        shotState = !shotState;
-        battleUI.ShotOrMove(shotState);
-    }
+        //void ChangeShotState()
+        //{
+        //    shotState = !shotState;
+        //    battleUI.ShotOrMove(shotState);
+        //}
 
-    CharacterController[] GetCharacters()
+        CharacterController[] GetCharacters()
     {
         return characters.Select(person => person.GetComponent<CharacterController>()).ToArray();
     }
@@ -47,6 +49,77 @@ public class BattleManager : MonoBehaviour
         {
             character.SetChoosenOne(false);
         }
+    }
 
+    bool CheckClose(Vector3 character, Vector3 against, float range)
+    {
+        return (Vector3.Distance(character, against) <= range);
+    }
+
+    bool CheckCastClose(GameObject character, GameObject against)
+    {
+        return CheckClose(character.transform.position, against.transform.position, character.GetComponent<CharacterController>().CastRange);
+    }
+
+    bool CheckAttackClose(GameObject character, GameObject against)
+    {
+        return CheckClose(character.transform.position, against.transform.position, character.GetComponent<CharacterController>().AttackRange);
+    }
+
+    bool CheckSightClose(GameObject character, GameObject against)
+    {
+        return CheckClose(character.transform.position, against.transform.position, character.GetComponent<CharacterController>().SightRange);
+    }
+
+    List<GameObject> GetAvailableToCast(GameObject character, GameObject[] against)
+    {
+        List<GameObject> available = new List<GameObject>();
+        for (int i = 0; i < against.Length; ++i)
+            if (CheckCastClose(character, against[i]))
+                available.Add(against[i]);
+
+        return available;
+    }
+
+    void ShowAvailableToCast(GameObject character, GameObject[] against)
+    {
+        List<int> available = new List<int>();
+        for (int i = 0; i < against.Length; ++i)
+        {
+            if (CheckCastClose(character, against[i]))
+                available.Add(i);
+        }
+
+        battleUI.ShowEnemyButtons(available.ToArray());
+    }
+
+    void Update()
+    {
+        SetCastAvailable();
+
+    }
+
+    void SetCastAvailable()
+    {
+        var activeChars = ActiveCharacters.Where(c => c.isChoosenOne).ToArray();
+        if (activeChars.Length > 0) //.Aggregate((_, x) => x.isChoosenOne ? x : _).gameObject;
+        {
+            var activeChar = activeChars.First().gameObject;
+            var arr = InactiveCharacters.Select(person => person.gameObject).ToArray();
+            //List<GameObject> availableToCast = GetAvailableToCast(activeChar, arr);
+            ShowAvailableToCast(activeChar, arr);
+            //if (availableToCast.Count > 0)
+            //{
+            //    var colors = battleUI.shotButton.colors;
+            //    colors.normalColor = Color.red;
+            //    battleUI.shotButton.colors = colors;
+            //}
+            //else
+            //{
+            //    var colors = battleUI.shotButton.colors;
+            //    colors.normalColor = Color.white;
+            //    battleUI.shotButton.colors = colors;
+            //}
+        }
     }
 }
